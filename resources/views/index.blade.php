38cf78714d5603d651779a7c7eab2d1b0808dc6d
igi-e <62782368+igi-e@users.noreply.github.com>
2020-09-24 01:02:05 +0900

    commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>(有)猪木酸素玉島店</title>
+</head>
+<body>
+    <header>
+
+            <img src="../resources./public/img/logo.jpg" art="(有)猪木酸素玉島店">
+        
+        </div>
+    </header>
+</body>
+</html>
